148ca0f00d01f35965ccb8fa0b22b658811c02b7
Davi Dolabella <davimdolabella03@gmail.com>
2024-08-03 10:39:01 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,10 +20,10 @@ window.onload = () =>{
             tela_velha.style.display = 'flex';
         }else if(valor == 'x'){
             tela_vitoria.style.display = 'flex';
-            peca_vitoria.style.backgroundImage = "url('../img/o.png')";
+            peca_vitoria.style.backgroundImage = "url('./img/o.png')";
         }else if(valor == 'o'){
             tela_vitoria.style.display = 'flex';
-            peca_vitoria.style.backgroundImage = "url('../img/x.png')";
+            peca_vitoria.style.backgroundImage = "url('./img/x.png')";
         }
         for(let i = 0; i < 2; i++){
             continuar[i].onclick = () =>{
